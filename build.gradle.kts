f418faff907ed2543a3f1562f461db3cb53175ee
KhakiKukhi <a@a.a>
2026-02-21 21:43:03 +0100

    Update Gateway dependencies and configuration for WebFlux compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webflux")
     implementation("org.springframework.boot:spring-boot-starter")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
