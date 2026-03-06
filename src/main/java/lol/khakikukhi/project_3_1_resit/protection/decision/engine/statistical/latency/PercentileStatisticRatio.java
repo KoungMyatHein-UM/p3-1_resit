2d6e7c84f22dc1ea2c91b69da80206e74be81358
KhakiKukhi <a@a.com>
2026-03-06 14:18:44 +0100

    Rename `PercentilePair` to `PercentileStatisticRatio` and refactor methods to improve visibility.

--- src/main/java/lol/khakikukhi/project_3_1_resit/protection/decision/engine/statistical/latency/PercentileStatisticRatio.java
+++ src/main/java/lol/khakikukhi/project_3_1_resit/protection/decision/engine/statistical/latency/PercentileStatisticRatio.java
@@ -2,19 +2,16 @@ package lol.khakikukhi.project_3_1_resit.protection.decision.engine.statistical.
 
 import lol.khakikukhi.project_3_1_resit.protection.decision.engine.statistical.latency.rolling.PercentileStatistic;
 
-//ratio < 2 → ALLOW
-//ratio < 5 → DEGRADE
-//ratio ≥ 5 → REJECT
-public class PercentilePair {
+public class PercentileStatisticRatio {
     private final PercentileStatistic p50Statistic;
     private final PercentileStatistic p95Statistic;
 
-    public PercentilePair(int windowSize50, int windowSize95) {
+    public PercentileStatisticRatio(int windowSize50, int windowSize95) {
         p50Statistic = new PercentileStatistic(windowSize50, 0.5);
         p95Statistic = new PercentileStatistic(windowSize95, 0.95);
     }
 
-    double getRatio() {
+    public double getRatio() {
         double p50 = p50Statistic.get();
         double p95 = p95Statistic.get();
 
@@ -25,7 +22,7 @@ public class PercentilePair {
         return p95 / p50;
     }
 
-    void update(double value) {
+    public void update(double value) {
         p50Statistic.update(value);
         p95Statistic.update(value);
 
