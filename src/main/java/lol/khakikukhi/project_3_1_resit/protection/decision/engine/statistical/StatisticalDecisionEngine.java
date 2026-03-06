faa62f4354995eb9c2c34c822fa0ff77badf8726
KhakiKukhi <a@a.com>
2026-03-06 14:18:52 +0100

    Refactor StatisticalDecisionEngine to replace PercentilePair with PercentileStatisticRatio; implement decision logic based on latency ratio.

--- src/main/java/lol/khakikukhi/project_3_1_resit/protection/decision/engine/statistical/StatisticalDecisionEngine.java
+++ src/main/java/lol/khakikukhi/project_3_1_resit/protection/decision/engine/statistical/StatisticalDecisionEngine.java
@@ -4,8 +4,7 @@ import lol.khakikukhi.project_3_1_resit.protection.decision.Decision;
 import lol.khakikukhi.project_3_1_resit.protection.RequestContext;
 import lol.khakikukhi.project_3_1_resit.protection.ResponseContext;
 import lol.khakikukhi.project_3_1_resit.protection.decision.engine.CoreDecisionEngine;
-import lol.khakikukhi.project_3_1_resit.protection.decision.engine.statistical.latency.PercentilePair;
-import lol.khakikukhi.project_3_1_resit.protection.decision.engine.statistical.latency.rolling.WindowMeanStatistic;
+import lol.khakikukhi.project_3_1_resit.protection.decision.engine.statistical.latency.PercentileStatisticRatio;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -14,15 +13,36 @@ import java.util.concurrent.ConcurrentHashMap;
 @Component
 public class StatisticalDecisionEngine extends CoreDecisionEngine {
 
-    private final Map<String, PercentilePair> endpointLatencies = new ConcurrentHashMap<>();
+    private final Map<String, PercentileStatisticRatio> endpointLatencies = new ConcurrentHashMap<>();
 
     @Override
     public Decision handleDecide(RequestContext context) {
-        return Decision.DEGRADE; // FOR TESTING ONLY
+        String path = context.path();
+        PercentileStatisticRatio latencyStatistic = endpointLatencies.computeIfAbsent(path, k -> new PercentileStatisticRatio(1000, 200));
+        return makeDecision(latencyStatistic.getRatio());
     }
 
     @Override
     public void feedback(RequestContext context, ResponseContext response, Decision decision) {
+        String path = context.path();
+        double latency = response.latencyMillis();
 
+        PercentileStatisticRatio latencyStatistic = endpointLatencies.computeIfAbsent(path, k -> new PercentileStatisticRatio(1000, 200));
+        latencyStatistic.update(latency);
     }
+
+    private Decision makeDecision(double latencyRatio) {
+        //ratio < 1.5 → ALLOW
+        //ratio < 2 → DEGRADE
+        //ratio ≥ 2 → REJECT
+
+        if (latencyRatio < 1.5) {
+            return Decision.ALLOW;
+        } else if (latencyRatio < 2) {
+            return Decision.DEGRADE;
+        } else {
+            return Decision.REJECT;
+        }
+    }
+
 }
